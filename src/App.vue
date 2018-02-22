5e2ecb7e8e04afee6f901a6c65d9d3f2906c36a6
zhe.zhang <zhe.zhang@cinsightech.com>
2018-02-22 18:11:30 +0800

    张哲sell app1.0

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<!-- <template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style> -->
+<template>
+  <div>
+    <v-header></v-header>
+    <v-goods></v-goods>
+    <div class="tab border-1px">
+      <div class="tab-item">
+        <router-link :to="{name:'goods',params:{num:123}}">商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/ratings">评论</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/seller">商家</router-link>
+      </div>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import header from './components/header/header.vue';
+  import goods from './components/goods/goods.vue';
+  export default{
+      components: {
+        'v-header': header,
+        'v-goods': goods
+      }
+  };
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "./common/stylus/mixin.styl"
+  .tab
+    display: flex
+    width: 100%
+    height: 40px
+    line-height: 40px
+    border-1px(rgba(7,17,27,0.1))
+    .tab-item
+      flex: 1
+      text-align: center
+      & > a
+        display: block
+        font-size: 14px
+        color: rgb(77, 85, 93)
+        &.active
+          color: rgb(240, 20, 20)
+</style>
